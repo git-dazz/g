71c18b7e9aeb6f0c33c806fb76186de92daa49b8
git-dazz <dazzler2022@qq.com>
2024-07-30 22:14:04 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,17 +10,18 @@ async function fetchText(url, opt) {
     console.info(url);
     let gap = "  ";
     let resp = await fetch(url, opt || {});
+    let ct = resp.headers.get("Content-Type");
     let txt = await resp.text();
     console.log(gap, resp.status, resp.statusText);
     console.log(gap, txt, "\n");
-    return txt;
+    return {txt,ct};
 }
 
 app.get('/*',async (c) => {
   // https://raw.githubusercontent.com/git-dazz/dist/main/deno/fast.ts
   let url = `https://raw.githubusercontent.com`+c.req.path;
-  let txt = await fetchText(url);
-  return c.text(''+txt);
+  let {txt,ct} = await fetchText(url);
+  return c.text(ct+''+txt);
 })
 
 Deno.serve(app.fetch)
