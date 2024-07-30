e5e7f519b1bc705ec2f647be2065c9569e317021
git-dazz <dazzler2022@qq.com>
2024-07-30 22:02:05 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,23 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
+  
   return c.text('Hello Hono!')
+})
+async function fetchText(url, opt) {
+    console.info(url);
+    let gap = "  ";
+    let resp = await fetch(url, opt || {});
+    let txt = await resp.text();
+    console.log(gap, resp.status, resp.statusText);
+    console.log(gap, txt, "\n");
+    return txt;
+}
+
+app.get('/*', (c) => {
+  // https://raw.githubusercontent.com/git-dazz/dist/main/deno/fast.ts
+  
+  return c.text(''+c.req.url)
 })
 
 Deno.serve(app.fetch)
